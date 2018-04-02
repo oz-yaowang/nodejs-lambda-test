94093cba61f1888b1c9d84f7d9f74f2f5b31a375
Yao Wang <yao.wang@hrx.ai>
2018-04-02 11:24:01 +0800

    no message

--- test/test.js
+++ test/test.js
@@ -1,17 +1,17 @@
-// let LambdaTester = require('lambda-tester');
+let LambdaTester = require('lambda-tester');
 let lambda = require('../src/lambda').handler;
-// let assert = require('assert');
+let assert = require('assert');
 
 /* global describe:true it:true*/
 describe('test lambda function', function () {
 
 
-    // it('call Lambda.js callback', function () {
-    //     return LambdaTester(lambda).event({content: 'test'}).expectResult((result) => {
-    //         assert.equal(result, 'some success message');
-    //     });
-    //
-    // });
+    it('call Lambda.js callback', function () {
+        return LambdaTester(lambda).event({content: 'test'}).expectResult((result) => {
+            assert.equal(result, 'some success message');
+        });
+
+    });
 
     it('call Lambda.js null callback branch', function () {
         lambda();
